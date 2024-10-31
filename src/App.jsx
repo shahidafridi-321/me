ec1382628d9a8e11fa27e4de37a642b8e20ea413
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-31 16:02:31 +0500

    Set up enviroment

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const App = () => {
+	return <div className="bg-redForBody text-white">this is just testing</div>;
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				redForBody: "#000000",
+			},
+		},
 	},
 	plugins: [],
 };
