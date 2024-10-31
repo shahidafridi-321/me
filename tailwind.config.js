34baa33b30b2b440062e079b6039304615a22ddb
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-31 17:00:43 +0500

    Add custom break points

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
 	content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
 	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+			"2xl": "1536px",
+		},
 		extend: {
 			colors: {
 				redForBody: "#000000",
