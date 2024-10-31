c3533431c309adc069690acace7039fd4da0bdd4
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-31 17:01:10 +0500

    Apply Custom styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export const Header = ({ titleText }) => {
 	return (
 		<header className="bg-gray-900 w-screen h-auto px-2 py-5 text-center">
-			<h1 className="text-white text-big-title">{titleText}</h1>
+			<h1 className="text-white text-lg sm:text-big-title">
+				{titleText}
+			</h1>
 		</header>
 	);
 };
