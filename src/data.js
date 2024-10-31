2a2d81c5cfb353806296e7042b134f90eedfa33b
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-31 22:03:44 +0500

    Add some dummy data for visualizing

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const charactersList = [
+	{ id: 0, title: "Don", image: "../public/100faces.jpg" },
+	{ id: 1, title: "Shahid", image: "../public/accommodation.jpg" },
+	{ id: 2, title: "Afridi", image: "../public/charles street.jpg" },
+	{ id: 3, title: "Uzair", image: "../public/Engineering student cta.jpg" },
+	{ id: 4, title: "Ishaq", image: "../public/HenryBootConstruction.jpg" },
+	{ id: 6, title: "Ali", image: "../public/SCYHeR launch_web.jpg" },
+	{ id: 7, title: "Tom", image: "../public/grass-abstract.jpg" },
+	{ id: 8, title: "John", image: "../public/charles street.jpg" },
+	{ id: 9, title: "Cat", image: "../public/pic.png" },
+	{ id: 10, title: "Kat", image: "../public/lykke cafe.jpg" },
+	{ id: 11, title: "Manu", image: "../public/owen-sky.jpg" },
+	{ id: 12, title: "Sunny", image: "../public/pic.png" },
+];
