394d925a4daf7316984830462893731412fbb96e
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-11-01 18:22:43 +0500

    Remove the bestScore feild

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -2,7 +2,6 @@ export const ScoreCard = ({ score, bestScore }) => {
 	return (
 		<div className="bg-gray-500 flex flex-col p-4 shadow-red-950 max-w-44 rounded-md">
 			<p className="text-gray-200 text-xl">Score : {score}</p>
-			<p className="text-gray-200 text-lg">Best Score : {bestScore}</p>
 		</div>
 	);
 };
